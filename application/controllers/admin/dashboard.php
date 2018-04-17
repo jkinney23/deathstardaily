dcc0d7e56624dd1585207bb70cc77581df5f1161
Jason Kinney <jkinney23@yahoo.ca>
2018-04-17 12:42:34 +0300

    start admin style

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -1,1 +1,15 @@
 <?php
+class Dashboard extends Admin_Controller
+{
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		$this->load_>view('_layout_main');
+	}
+	
+	public function modal() {
+		$this->load->view('_layout_modal');
+	}
+}
--- application/controllers/page.php
+++ application/controllers/page.php
@@ -7,22 +7,6 @@ class Page extends Frontend_Controller {
 	}
 	
 	public function index() {
-		$pages = $this->page_m->get_by(array('slug' => 'about'));
-		var_dump($pages);
-	}
-	
-	public function save() {
-		$data = array(
-			//'title' => 'My page',
-			//'slug' => 'my-page',
-			'order' => '3',
-			//'body' => 'This is my body...'
-		);
-		$id = $this->page_m->save($data, 5);
-		var_dump($id);
-	}
-	
-	public function delete() {
-		$this->page_m->delete(5);
+
 	}
 }
