c0f0286eb72003c01c5f59e5676ad1f14edb701f
Jason Kinney <jkinney23@yahoo.ca>
2018-04-19 17:55:37 +0300

    complete page controller

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -6,10 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    
+    <!--
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	-->
+	<link href="<?php echo site_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!--
 	<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+	-->
+	<script type="text/javascript" src="<?php echo site_url('js/tinymce/tinymce.min.js'); ?>"></script>
+	
 	<script>tinymce.init({ selector:'textarea' });</script>    
 	<title><?php echo $meta_title; ?></title>
   </head>
